2ca61e1db7272ee68436f6091cbfc42edb590639
小黎 <22708206@qq.com>
2023-09-19 20:56:52 +0800

    fix(main.ts): 补充ElMessage样式文件
    补充ElMessage样式文件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/index.less'
+import 'element-plus/es/components/message/style/css'
 
 const app = createApp(App)
 app.mount('#app')
